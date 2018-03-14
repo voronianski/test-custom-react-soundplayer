908eca5d1df0875d3b6f3c6e9f00cf444a4a6448
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2018-03-14 11:45:36 +0100

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Test Custom React Soundplayer
+
+```bash
+npm i
+npm start
+# open `index.html` in browser
+```
+
+---
---
+++ index.js
@@ -0,0 +1,49 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const {
+  PlayButton,
+  Timer
+} = require('react-soundplayer/components');
+const {
+  withSoundCloudAudio
+} = require('react-soundplayer/addons');
+
+const streamUrl='https://s3-eu-west-1.amazonaws.com/react-soundplayer-examples/ksmtk-reborn-edit.mp3'
+
+const MusicPlayer = withSoundCloudAudio(props => {
+  return (
+    <div className="music-player">
+      <h2>{props.data.artist} - {props.data.title}</h2>
+      <PlayButton
+        onTogglePlay={() => {
+          console.log('play button clicked!')
+        }}
+        {...props} />
+      <Timer {...props} />
+    </div>
+  );
+});
+
+const AlbumDetail = props => {
+  return (
+    <div className="album-detail">
+      <div>Album detail info</div>
+      <MusicPlayer {...props} />
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>App!</h1>
+      <AlbumDetail
+        data={{title: 'Reborn', artist: 'Ksmtk'}}
+        streamUrl={streamUrl}
+        preloadType="auto" />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
